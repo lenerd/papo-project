38ceceac6804ec0ee526776f316561fc76ae90f9
Armin Schaare <armin-scha@hotmail.de>
2015-06-02 18:29:43 +0200

    Added edge_count function

--- code/src/neuralnet/neuralnet.c
+++ code/src/neuralnet/neuralnet.c
@@ -1,36 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
 #include "neuralnet.h"
+#include "math_ext.h"
 
-float sigmoid(float x)		   { return 1.0f / (1.0f + (float)exp(x)); }
-float inverse_sigmoid(float x) { return (float)(-log(1.0f / x - 1.0f)); }
-
-float centered_sigmoid(float x) { return 2.0f * (sigmoid(x) - 0.5f); }
-
-float random_value_01()					    { return (float)rand() / RAND_MAX; }
-float random_value_0m(float max)			{ return max * rand() / RAND_MAX; }
-float random_value_mm(float min, float max) { return min + (max - min) * rand() / RAND_MAX; }
+int edge_count(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count){
+	return (input_count + 1) * neurons_per_hidden_layer +
+		(neurons_per_hidden_layer + 1) * neurons_per_hidden_layer * (hidden_layer_count - 1) +
+		neurons_per_hidden_layer * output_count;
+}
 
 struct neuralnet* allocate_neural_net(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count){
 	
-	struct neuralnet* n = malloc(sizeof(struct neuralnet));
+	struct neuralnet* n = (struct neuralnet*)malloc(sizeof(struct neuralnet));
 
 	n->input_count = input_count;
 	n->hidden_layer_count = hidden_layer_count;
 	n->neurons_per_hidden_layer = neurons_per_hidden_layer;
 	n->output_count = output_count;
 
-	n->edges_count = (input_count + 1) * neurons_per_hidden_layer + 
-		(neurons_per_hidden_layer + 1) * neurons_per_hidden_layer * (hidden_layer_count - 1) + 
-		neurons_per_hidden_layer * output_count;
+	n->edges_count = edge_count(input_count, hidden_layer_count, neurons_per_hidden_layer, output_count);
 
 	n->edges = (float*)malloc(sizeof(float) *(n->edges_count));
 
 	return n;
 
 }
+
 void initialize_neural_net_random(struct neuralnet* net){
 	
 	for (int i = 0; i < net->edges_count; i++){
@@ -48,6 +43,7 @@ void initialize_neural_net_buffer(struct neuralnet* net, float* edges){
 void initialize_neural_net_data(struct neuralnet* net, char* filepath){
 	//TODO: implement
 }
+
 struct neuralnet* create_neural_net_random(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count){
 	
 	struct neuralnet* net = allocate_neural_net(input_count, hidden_layer_count, neurons_per_hidden_layer, output_count);
@@ -76,6 +72,7 @@ void deallocate_neural_net(struct neuralnet* net){
 	free(net);
 	
 }
+
 void destroy_neural_net(struct neuralnet* net){
 	deallocate_neural_net(net);
 }
@@ -167,54 +164,4 @@ void print_neural_net(const struct neuralnet* net){
 	}
 	printf("\n\n\n");
 
-}
-
-//Testing and Debugging
-int main(int argc, char** argv){
-	srand((unsigned int)time(NULL));
-
-	
-
-	//1 input, 1 hidden layer, 1 neurons per hidden layer, 1 output.
-	struct neuralnet* n1 = allocate_neural_net(1, 1, 1, 1);
-	float* edges = (float*)malloc(sizeof(float) * n1->edges_count);
-	edges[0] = 0.0f; 
-	edges[1] = 1.0f; 
-	edges[2] = 1.0f;
-	initialize_neural_net_buffer(n1, edges);
-	free(edges);
-
-	print_neural_net(n1);
-	
-	float n1_in[] = { 1.0f };
-	float n1_out[1];
-
-	calculate_output(n1, n1_in, n1_out);
-	printf("Input: %f\nOutput: %f\n\n", n1_in[0], n1_out[0]);
-
-	//3 input, 4 hidden layer, 6 neurons per hidden layer, 2 output.
-	struct neuralnet* n2 = create_neural_net_random(3, 4, 6, 2);
-
-	print_neural_net(n2);
-
-	float n2_in[] = { 0.0f, 0.0f, 0.0f };
-	float n2_out[2];
-
-	calculate_output(n2, n2_in, n2_out);
-	printf("Input: %f, %f, %f\nOutput: %f, %f\n\n", n2_in[0], n2_in[1], n2_in[2], n2_out[0], n2_out[1]);
-
-	//1 input, 1 hidden layer, 1 neurons per hidden layer, 1 output.
-	struct neuralnet* n3 = create_neural_net_random(1, 1, 1, 1);
-
-	print_neural_net(n3);
-
-	float n3_in[] = { 0.0f };
-	float n3_out[1];
-
-	calculate_output(n3, n3_in, n3_out);
-	printf("Input: %f\nOutput: %f\n\n", n3_in[0], n3_out[0]);
-
-	getchar();
-
-	return 0;
 }
--- code/src/neuralnet/neuralnet.h
+++ code/src/neuralnet/neuralnet.h
@@ -1,14 +1,6 @@
 #ifndef NEURALNET_H
 #define NEURALNET_H
 
-float sigmoid(float x);							//Mathematical sigmoid function for the intensity of firing neurons. Output values are between 0.0 and 1.0, exclusive. Cuts the y-Axis at 0.5.
-float inverse_sigmoid(float x);					//Inverse of the sigmoid function for initial edge weights of the neuralnet (x has to be between 0.0 and 1.0, exclusive)
-float centered_sigmoid(float x);				//Centered version of the Sigmoid function. Output values range from -1.0 to 1.0 (exclusive) with the center of the function at (0|0).
-
-float random_value_01();						//Random Value from 0.0 to 1.0.
-float random_value_0m(float max);				//Random Value from 0.0 to max.
-float random_value_mm(float min, float max);	//Random Value from min to max.
-
 struct neuralnet{
 	int edges_count;
 
@@ -20,10 +12,15 @@ struct neuralnet{
 	float* edges;
 };
 
+//Returns how many edges a neural net has, based on its preferences.
+int edge_count(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count);
+
 struct neuralnet* allocate_neural_net(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count);
+
 void initialize_neural_net_random(struct neuralnet* net);
 void initialize_neural_net_buffer(struct neuralnet* net, float* buffer);
 void initialize_neural_net_data(struct neuralnet* net, char* filepath);
+
 struct neuralnet* create_neural_net_random(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count);
 struct neuralnet* create_neural_net_buffer(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count, float* edges);
 struct neuralnet* create_neural_net_data(int input_count, int hidden_layer_count, int neurons_per_hidden_layer, int output_count, char* filepath);
