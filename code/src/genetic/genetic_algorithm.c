87abd7dd657573f5f5283d4387dc29123609a85e
Armin Schaare <armin-scha@hotmail.de>
2015-06-02 18:29:24 +0200

    Last commit were wrong Versions

--- code/src/genetic/genetic_algorithm.c
+++ code/src/genetic/genetic_algorithm.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
-#include "rand_ext.h"
+#include "math_ext.h"
 #include "genetic_algorithm.h"
+
+mutation_crossover_ratio = 0.3f;
+gene_mutation_chance = 0.001f;
 
 struct genome* create_genome(int genes_count, float* genes){
 
@@ -26,18 +29,53 @@ struct population* create_population(int population_size, int genome_size, float
 
 }
 
-void mutate_genome(struct genome* gen){
+struct genome* mutate_genome(struct genome* gen){
+
+	float* new_genes = (float*)malloc(sizeof(float) * gen->genes_count);
 
 	for (int i = 0; i < gen->genes_count; i++){
+		new_genes[i] = gen->genes[i];
 		if (random_value_01() < gene_mutation_chance){
-			gen->genes[i] += 
+			new_genes[i] += inverse_sigmoid(random_value_01());
 		}
 	}
 
+	return create_genome(gen->genes_count, new_genes);
+
+}
+struct genome* crossover_genomes(struct genome* father, struct genome* mother){
+	
+	if (father->genes_count != mother->genes_count)
+		return father;
+
+	int border = (int)random_value_0m(father->genes_count);
+
+	float* new_genes = (float*)malloc(sizeof(float) * father->genes_count);
+
+	for (int i = 0; i < father->genes_count; i++){
+		new_genes[i] = i > border ? mother->genes[i] : father->genes[i];
+	}
+
+	return create_genome(father->genes_count, new_genes);
+
 }
 
-int main(int argc, char** argv){
+struct genome* select_individual(struct population* pop){
+	
+	float r = random_value_01() * (pop->total_fitness + pop->size);
+	float f = 0.0f;
+	int i = 0;
+	for (; i < pop->size; i++){
+		f += pop->individuals[i]->fitness + 1.0f;
+		if (f > r) { break; }
+	}
+	return pop->individuals[i];
 
+}
+void next_generation(struct population* pop){
 
+	for (int i = 0; i < pop->size; i++){
+		//pop->individuals[i]
+	}
 
 }
--- code/src/genetic/genetic_algorithm.h
+++ code/src/genetic/genetic_algorithm.h
@@ -15,15 +15,21 @@ struct population{
 	struct genome** individuals;
 
 	int generation;
+	float total_fitness;
+	float avg_fitness;
 
 };
 
+float mutation_crossover_ratio;
 float gene_mutation_chance;
 
 struct genome* create_genome(int genes_count, float* genes);
 struct population* create_population(int population_size, int genome_size, float** genes);
 
-void mutate_genome(struct genome* gen);
-void crossover_genomes(struct genome* father, struct genome* mother);
+struct genome* mutate_genome(struct genome* gen);
+struct genome* crossover_genomes(struct genome* father, struct genome* mother);
+
+struct genome* select_individual(struct population* pop);
+void next_generation(struct population* pop);
 
 #endif /* GENETIC_ALGORITHM_H */
