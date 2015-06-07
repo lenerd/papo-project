aefc346ab3724a5eeeca6e951ec48b20524d0e83
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-06-07 18:17:06 +0200

    revised random float generation

--- code/src/math_extend/math_ext.c
+++ code/src/math_extend/math_ext.c
@@ -4,18 +4,50 @@
 #include "math_ext.h"
 
 
-void set_seed() { srand((unsigned int)time(NULL)); }
-void set_seed_ifn() {
-	if (seed_set == 0){
-		set_seed();
-		seed_set = 1;
-	}
+void set_seed (unsigned int seed)
+{
+    srand (seed);
 }
 
-float random_value_01()					    { set_seed_ifn(); return (float)rand() / RAND_MAX; }
-float random_value_0m(float max)			{ set_seed_ifn(); return max * rand() / RAND_MAX; }
-float random_value_mm(float min, float max) { set_seed_ifn(); return min + (max - min) * rand() / RAND_MAX; }
+void set_seed_time (void)
+{
+    srand ((unsigned int) time (NULL));
+}
 
-float sigmoid(float x)		   { return 1.0f / (1.0f + (float)exp(x)); }
-float inverse_sigmoid(float x) { return (float)(-log(1.0f / x - 1.0f)); }
-float centered_sigmoid(float x) { return 2.0f * (sigmoid(x) - 0.5f); }
+void set_seed_ifn ()
+{
+    if (!seed_set)
+    {
+        set_seed_time ();
+        seed_set = true;
+    }
+}
+
+float random_value_01 ()
+{
+    return random_value_mm (0.0f, 1.0f);
+}
+
+float random_value_0m (float max)
+{
+    return random_value_mm (0.0f, max);
+}
+
+float random_value_mm (float min, float max)
+{
+    set_seed_ifn ();
+    return min + (max - min) * (float) rand () / (float) RAND_MAX;
+}
+
+float sigmoid (float x)
+{
+    return 1.0f / (1.0f + (float) exp (x));
+}
+float inverse_sigmoid (float x)
+{
+    return (float) (-log (1.0f / x - 1.0f));
+}
+float centered_sigmoid (float x)
+{
+    return 2.0f * (sigmoid (x) - 0.5f);
+}
