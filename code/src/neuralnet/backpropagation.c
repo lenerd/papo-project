d7167eec5911c8357aad37761e909929211437d5
Theresa Eimer <t_eimer@rocketmail.com>
2015-07-03 15:55:07 +0200

    Added draft of a possible backpropagation algorithm

---
+++ code/src/neuralnet/backpropagation.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "neuralnet.h"
+
+float* backpropagation(neuralnet_t* net, int board_size)
+{
+	//Get training data for board size
+
+	//Do until error is small enough:
+		
+		//For each data point
+	
+			//calculate output
+			//calculate difference matrix between output and desired result (can probably be used as error)
+	
+			//For each hidden layer do:
+				//For each output value do:
+					//Split difference between output and result evenly
+					//Distribute the difference between the edge weights
+
+	
+	//Return new weights
+}
