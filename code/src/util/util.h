b70df089fa9a0d17151524e51bd0051f6d8f85bf
Armin Schaare <armin-scha@hotmail.de>
2015-07-06 17:32:41 +0200

    Added swap_buffer function.

--- code/src/util/util.h
+++ code/src/util/util.h
@@ -1,7 +1,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#define SAFEMALLOC(type, element_count) malloc (element_count * sizeof (type)); if (net == NULL) { fprintf (stderr, "malloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
-#define SAFECALLOC(type, element_count) calloc (element_count * sizeof (type)); if (net == NULL) { fprintf (stderr, "calloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
+#define SAFE_MALLOC(type, element_count) malloc ((element_count) * sizeof (type)); if (net == NULL) { fprintf (stderr, "malloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
+#define SAFE_CALLOC(type, element_count) calloc ((element_count) * sizeof (type)); if (net == NULL) { fprintf (stderr, "calloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
+
+void swap_buffer(void* b1, void* b2){
+	void* temp = b1;
+	b1 = b2;
+	b2 = temp;
+}
 
 #endif
