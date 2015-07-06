aadca3bf34a05b60545d2cdc7bd385ff6b0e7bed
Armin Schaare <armin-scha@hotmail.de>
2015-07-06 16:59:09 +0200

    Added Macro functions for safe malloc and calloc.

--- code/src/util/util.h
+++ code/src/util/util.h
@@ -1,6 +1,7 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-#define SAFEMALLOC()
+#define SAFEMALLOC(type, element_count) malloc (element_count * sizeof (type)); if (net == NULL) { fprintf (stderr, "malloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
+#define SAFECALLOC(type, element_count) calloc (element_count * sizeof (type)); if (net == NULL) { fprintf (stderr, "calloc() failed in file %s at line # %d\n", __FILE__, __LINE__); exit (EXIT_FAILURE); }
 
 #endif
