4fdb3fd0e039fed069f3979bba86588cb334d223
Theresa Eimer <t_eimer@rocketmail.com>
2015-08-02 13:17:56 +0200

    Some work on main

--- code/src/main.c
+++ code/src/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "genetic/genetic_algorithm.h"
 #include "newneunet/newneunet.h"
 #include "go/game_controller.h"
 #include "training/training.h"
+
+struct net_struct
+{
+	neuralnet_t* net;
+	int score;
+};
+
+int cmp(struct net_struct* a, struct net_struct* b)
+{
+	return a->score - b->score;
+}
 
 int main (int argc, char** argv)
 {
@@ -12,7 +24,7 @@ int main (int argc, char** argv)
 	{
 		//Setup
 		//Read arguments
-		int nets = atoi(argv[1]);
+		int net_count = atoi(argv[1]);
 		int generations = atoi(argv[2]);
 		int board_size = atoi(argv[3]);
 		int hidden_layers = atoi(argv[4]);
@@ -26,9 +38,7 @@ int main (int argc, char** argv)
 			komi = 4.5;
 					
 		//Create as many nets as specified or take the given population
-		neuralnet_t** population = malloc(sizeof(neuralnet_t*) * nets);
-		int input_count = board_size * board_size;
-		int output_count = input_count +1;
+		struct net_struct** current_nets = malloc((sizeof(neuralnet_t*) + sizeof(int)) * net_count);
 
 		if(argc >= 7)
 		{
@@ -36,51 +46,78 @@ int main (int argc, char** argv)
 		}
 		else
 		{
-			for(int i = 0; i < nets; ++i)
+			int* layers = malloc((hidden_layers +2)*sizeof(int));
+			layers[0] = board_size * board_size;
+			
+			for(int a = 1; a <= hidden_layers; ++a)
+			{	
+				layers[a] = neurons_per_layer;
+			}
+			layers[hidden_layers+2] = 2;
+
+			for(int i = 0; i < net_count; ++i)
 			{
-				population[i] = create_neural_net_random(0, NULL);
+				current_nets[i]->net = create_neural_net_random((hidden_layers+2), layers);
 			}
 		}
 	
 		//Train nets
 		struct dataset* data = generate_data(board_size, c_black);
-		//TODO Backpropagation 	
-		
+		//Backpropagation 	
+		for(int j = 0; j < net_count; ++j)
+		{
+			backpropagate(current_nets[j]->net, data->input_values, data->expected_values);	
+		}	
 			
 		//For as many times as specified
 		for(int i = 0; i < generations; ++i)
 		{	
-			float* scores = malloc(sizeof(int) * nets);
 
 			//Run games
-			for(int a = 0; a < nets; ++a)
+			for(int a = 0; a < net_count; ++a)
 			{
-				for(int b = a; b < nets; ++b)
+				for(int b = a; b < net_count; ++b)
 				{
 					if(b != a)
 					{
 						char name[4];
 						sprintf(name, "%c%c.sgf", a, b);
 						FILE* record = fopen(name, "a+");
-						result_t* result = play(board_size, population[a], population[b], komi, record);
+						result_t* result = play(board_size, current_nets[a]->net, current_nets[b]->net, komi, record);
 						fclose(record);
-						scores[a] += result->score_black;
-						scores[b] += result->score_black;
+						current_nets[a]->score += result->score_black;
+						current_nets[b]->score += result->score_black;
 					}		
 				}
 			}
 			
-			for(int a = 0; a < nets; ++a)
+			for(int a = 0; a < net_count; ++a)
 			{
-				printf("Score Netz %d: %d \n", a, scores[a]);
+				printf("Score Netz %d: %d \n", a, current_nets[a]->score);
 			}
-
+			
+			//Sort nets in descending order of scores 
+			qsort(current_nets, net_count, sizeof(struct net_struct), cmp);			
 			//Save best two nets
-			//Create next generation
+			
+			//Create population for genetic algorithm
+			genome_t** genomes = malloc(sizeof(genome_t) * net_count);
+			
+			for(int k = 0; k < net_count; ++k)
+			{
+				//genomes[k] = create_genome();
+			}			
+		
+			population_t* population = create_population(net_count, genomes, 0);
+		
+			//Make next generation	
+			next_generation(population);
+			
 		}
-
 		//At the end save all nets of the last generation
 	}
 
     return EXIT_SUCCESS;
 }
+
+
