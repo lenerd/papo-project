c1b4ef06db1c07e691878ad6a5e4d78868f6e8fb
Theresa Eimer <t_eimer@rocketmail.com>
2015-08-03 19:22:56 +0200

    Added bench stubs.

---
+++ code/bench/bench_backpropagation.c
@@ -0,0 +1,16 @@
+#include "newneunet/newneunet.h"
+#include <stdlib.h>
+
+/*
+ * Benchmark for the backpropagation
+ *
+ * Measures time: input/time
+ *
+ * Input size stays the same:
+ * 	 int 5 x 5
+ */
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
---
+++ code/bench/bench_genetic.c
@@ -0,0 +1,17 @@
+#include "genetic/genetic_algorithm.h"
+
+#include <stdlib.h>
+
+/*
+ * Benchmark for the genetic algorithm.
+ *
+ * Measures the time needed for x generations
+ *
+ * Genome size to be tested:
+ *	5 genes
+ */
+
+int main()
+{
+	return EXIT_SUCCESS;
+}
