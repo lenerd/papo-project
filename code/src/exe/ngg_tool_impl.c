67226a5d1d85383ea1ebeacb8aca9f1e24dfb2bd
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-08-13 00:18:40 +0200

    added training

--- code/src/exe/ngg_tool_impl.c
+++ code/src/exe/ngg_tool_impl.c
@@ -120,17 +120,41 @@ int train_networks (options_t* opts)
     if (ret)
         return ret;
 
-    nnet_set_t* set = nnet_set_from_file (opts->in_path, opts->b_in);
+    nnet_set_t* nets = nnet_set_from_file (opts->in_path, opts->b_in);
+    dataset_t* data = dataset_from_file (opts->training_data_path);
 
-    for (size_t i = 0; i < set->size; ++i)
+    for (size_t i = 0; i < nets->size; ++i)
     {
+        if (opts->verbose)
+            printf("begin with training of net %zu\n", i);
         for (size_t j = 0; j < opts->n; ++j)
         {
-            // TODO: new[j] <- train old[j]
+            for (size_t k = 0; k < data->size; ++k)
+            {
+                if (nets->nets[i]->neurons_per_layer[0] ==
+                        data->data[k]->input_size &&
+                    nets->nets[i]
+                            ->neurons_per_layer[nets->nets[i]->layer_count - 1] ==
+                        data->data[k]->output_size)
+                {
+                    nnet_backpropagate (nets->nets[i], data->data[k]->input,
+                                        data->data[k]->expected);
+                }
+                else
+                {
+                    fprintf (stderr, "input/output sizes of dataset does not match "
+                                     "i/o neurons\n");
+                }
+            }
         }
     }
+    if (opts->verbose)
+        printf("finished training\n");
 
-    nnet_set_to_file (set, opts->out_path, opts->b_out);
+    nnet_set_to_file (nets, opts->out_path, opts->b_out);
+
+    nnet_set_destroy (nets);
+    dataset_destroy (data);
 
     return ret;
 }
