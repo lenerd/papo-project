e438aaef7e372229f30202da79b65b21ed3110c9
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-08-24 22:57:49 +0200

    removed warnings in genetic test, assertions failing

--- code/tests/check_genetic.c
+++ code/tests/check_genetic.c
@@ -6,20 +6,23 @@
 
 void do_nothing(void* arg)
 {
+    int* dummy = (int*)arg;
+    ++dummy;
 }
 
 START_TEST(test_genome_type)
 {
-	int genes_count = 5;
-	float genes[5][4]= {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}, {17, 18, 19, 20}};
+	size_t genes_count = 20;
+	float genes[20]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    float* genes_p = genes;
 
-	genome_t* genome = genome_create(genes_count, genes, *do_nothing, 5);
+	genome_t* genome = genome_create(genes_count, &genes_p, &do_nothing, NULL);
 
 	ck_assert(genome != NULL);
 	ck_assert(genome->genes_count == genes_count);
-	ck_assert(genome->fitness == 0.0f);
-	ck_assert(genome->genes[2][3] == genes[2][3]);
-	ck_assert(genome->genes[2][2] == genes[2][2]);
+	ck_assert(!(genome->fitness < 0.0f) && !(genome->fitness > 0.0f));
+	ck_assert(!((*genome->genes)[2] < genes[2]) && !((*genome->genes)[2] > genes[2]));
+	ck_assert(!((*genome->genes)[3] < genes[3]) && !((*genome->genes)[3] > genes[3]));
 
 	genome_destroy(genome);	
 }
@@ -27,12 +30,13 @@ END_TEST
 
 START_TEST(test_population_type)
 {
-	int size = 7;
-	float genes[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	size_t size = 9;
+	float genes[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    float* genes_p = genes;
 
 	genome_t** genomes = SAFE_MALLOC(size*sizeof(genome_t*));
-	for(int i = 0; i < size; ++i)
-		genomes[i] = genome_create(5, genes, *do_nothing, 5);
+	for(size_t i = 0; i < size; ++i)
+		genomes[i] = genome_create(size, &genes_p, &do_nothing, NULL);
 
 	ck_assert(genomes != NULL);
 
@@ -40,11 +44,11 @@ START_TEST(test_population_type)
 
 	ck_assert(pop != NULL);
 	ck_assert(pop->size == size);
-	ck_assert(pop->base_fitness == 1);
-	ck_assert(pop->avg_fitness == 1);
-	ck_assert(pop->total_fitness = size);
-	ck_assert(pop->individuals[2]->genes[3][3] == genomes[2]->genes[3][3]);
-	ck_assert(pop->individuals[2]->genes[3][4] == genomes[2]->genes[3][4]);
+	ck_assert(!(pop->base_fitness < 1.0f) && !(pop->base_fitness > 1.0f));
+	ck_assert(!(pop->avg_fitness < 1.0f) && !(pop->avg_fitness > 1.0f));
+	ck_assert(pop->total_fitness = (float)size);
+	ck_assert(pop->individuals[2]->genes[3] == genomes[2]->genes[3]);
+	ck_assert(pop->individuals[2]->genes[4] == genomes[2]->genes[4]);
 
 	population_destroy(pop);
 	free(genomes);
@@ -54,16 +58,17 @@ END_TEST
 
 START_TEST(test_mutation)
 {
-	int genes_count = 2;
-	float genes[2][1] = {{2}, {3}};
+	size_t genes_count = 2;
+	float genes[2] = {2, 3};
+    float* genes_p = genes;
 
-	genome_t* genome = genome_create(genes_count, genes, *do_nothing, 1);
+	genome_t* genome = genome_create(genes_count, &genes_p, &do_nothing, NULL);
 
 	for(int i = 0; i < 1000; ++i)
 		mutate_genome(genome);
 
-	ck_assert(genome->genes[0][0] != genes[0][0]);
-	ck_assert(genome->genes[1][0] != genes[1][0]);
+	ck_assert(!((*genome->genes)[0] < genes[0]) && !((*genome->genes)[0] > genes[0]));
+	ck_assert(!((*genome->genes)[1] < genes[1]) && !((*genome->genes)[1] > genes[1]));
 
 	genome_destroy(genome);
 }
@@ -71,15 +76,18 @@ END_TEST
 
 START_TEST(test_selection)
 {
-	int size = 3;
-	float genes1[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	float genes2[3][3] = {{11, 12, 13}, {14, 15, 16}, {17, 18, 19}};
-	float genes3[3][3] = {{21, 22, 23}, {24, 25, 26}, {27, 28, 29}};	
+	size_t size = 9;
+	float genes1[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	float genes2[9] = {11, 12, 13, 14, 15, 16, 17, 18, 19};
+	float genes3[9] = {21, 22, 23, 24, 25, 26, 27, 28, 29};	
+    float* genes1_p = genes1;
+    float* genes2_p = genes2;
+    float* genes3_p = genes3;
 
 	genome_t** genomes = SAFE_MALLOC(size*sizeof(genome_t*));
-	genomes[0] = genome_create(5, genes1, *do_nothing, 5);
-	genomes[1] = genome_create(5, genes2, *do_nothing, 5);
-	genomes[2] = genome_create(5, genes3, *do_nothing, 5);
+	genomes[0] = genome_create(size, &genes1_p, &do_nothing, NULL);
+	genomes[1] = genome_create(size, &genes2_p, &do_nothing, NULL);
+	genomes[2] = genome_create(size, &genes3_p, &do_nothing, NULL);
 
 	ck_assert(genomes != NULL);
 
@@ -97,9 +105,9 @@ START_TEST(test_selection)
 	{
 		genome_t* ind = select_individual(pop);
 		
-		if(ind == genes1)
+		if(*ind->genes == genes1)
 			++count1;
-		else if(ind == genes2)
+		else if(*ind->genes == genes2)
 			++count2;
 		else
 			++count3;
@@ -145,6 +153,7 @@ int main (void)
 
 	sr = srunner_create (check_genetic_algorithm());
 	
+    srunner_set_fork_status (sr, CK_NOFORK);
 	srunner_run_all(sr, CK_VERBOSE);
 
 	n = srunner_ntests_failed (sr);
