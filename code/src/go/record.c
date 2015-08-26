cef1d2391b09ef3935e61ea73d1bd88f73bf6a55
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-08-26 17:41:31 +0200

    format record

--- code/src/go/record.c
+++ code/src/go/record.c
@@ -3,38 +3,39 @@
 #include <stdlib.h>
 
 
-FILE* create_file(const char* file_name)
+FILE* create_file (const char* file_name)
 {
-	FILE* record = fopen(file_name, "a");
+    FILE* record = fopen (file_name, "a");
     if (record == NULL)
     {
         fprintf (stderr, "fopen() failed in file %s at line # %d", __FILE__,
                  __LINE__);
         exit (EXIT_FAILURE);
     }
-	
-	// Programm
-	fprintf(record, "AP[nugengo:?] \n");
-	
-	// Game Type
-	fprintf(record, "GM[1] \n");
 
-	// Board size
-	fprintf(record, "SZ[9] \n");
-	
-	// fclose(record);	
+    // Programm
+    fprintf (record, "AP[nugengo:?] \n");
+
+    // Game Type
+    fprintf (record, "GM[1] \n");
+
+    // Board size
+    fprintf (record, "SZ[9] \n");
+
+    // fclose(record);
     return record;
 }
 
-void write_move(FILE* record, color_t color, uint8_t x, uint8_t y){
-	
-	char i = (char) ('a' + x);		
-	char j = (char) ('a' + y);
-	if(color == c_black){
-		fprintf(record, "B[%c%c]\n", i, j);
-	}
-	else{
-		fprintf(record, "W[%c%c]\n", i, j);		
-	}
+void write_move (FILE* record, color_t color, uint8_t x, uint8_t y)
+{
+    char i = (char) ('a' + x);
+    char j = (char) ('a' + y);
+    if (color == c_black)
+    {
+        fprintf (record, "B[%c%c]\n", i, j);
+    }
+    else
+    {
+        fprintf (record, "W[%c%c]\n", i, j);
+    }
 }
-
--- code/src/go/record.h
+++ code/src/go/record.h
@@ -17,7 +17,7 @@
 FILE* create_file (const char* file_name);
 
 /**
- * \brief writes a move to the .sgf
+ * \brief Writes moves into the created file (only use for legal moves without passing!)
  */
 void write_move (FILE* record, color_t color, uint8_t x, uint8_t y);
 
