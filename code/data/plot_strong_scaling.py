b141ad07f749f893d2ebeb46da6946db5edf3f78
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-09-01 18:34:24 +0200

    updated plotting script

--- code/data/plot_strong_scaling.py
+++ code/data/plot_strong_scaling.py
@@ -5,9 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(data, out):
+def plot_time(data, out):
     speedup = data[0, 2] / data[:, 2]
     efficiency = speedup / data[:, 0]
+
+    plt.close()
 
     # speedup per process
     plt.subplot(2, 1, 1)
@@ -30,15 +32,33 @@ def plot(data, out):
     plt.savefig(out, papertype='a4')
 
 
+def plot_moves(data, out):
+    moves = data[0, 4] + data[0, 5]
+    moves_per_s = moves / data[:, 2]
+
+    plt.close()
+
+    # moves/s
+    plt.plot(data[:, 0], moves_per_s, 'x')
+    plt.plot(data[:, 0], moves_per_s[0] * data[:, 0])
+    plt.title("moves/s")
+    plt.xlabel("# Processes")
+    plt.ylabel("Moves/s")
+    plt.legend(["measurements", "linear"], loc='best')
+
+    plt.tight_layout()
+    plt.savefig(out, papertype='a4')
+
+
 def main():
     if len(sys.argv) < 2:
         print("need csv file as cli parameter")
         sys.exit(1)
 
     data = np.loadtxt(sys.argv[1], delimiter=',')
-    out = 'strong_scaling.pdf'
 
-    plot(data, out)
+    plot_time(data, 'strong_scaling_time.pdf')
+    plot_moves(data, 'strong_scaling_moves.pdf')
 
 
 if __name__ == '__main__':
