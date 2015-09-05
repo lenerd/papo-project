6ed8fe8d1722b6f96d76d2f7454b0a66fd0e3e3c
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-09-05 16:17:22 +0200

    weak scaling data

--- code/data/plot_weak_scaling.py
+++ code/data/plot_weak_scaling.py
@@ -8,28 +8,32 @@ import matplotlib.pyplot as plt
 
 
 def plot_time(data, out):
-    speedup = data[0, 3] / data[:, 3]
+    speedup = [d[1][0, 3] / d[1][:, 3] for d in data]
 
     fig = plt.figure()
 
     # speedup per process
     ax_sp = fig.add_subplot(1, 1, 1)
-    ax_sp.plot(data[:, 0], speedup, 'x')
+    for d, s in zip(data, speedup):
+        ax_sp.plot(d[1][:, 0], s, 'x')
     ax_sp.set_title("Speedup")
     ax_sp.set_xlabel("# Processes")
     ax_sp.set_ylabel("Speedup")
-    ax_sp.legend(["measurements"], loc='best')
+    legend = [d[0] for d in data]
+    ax_sp.legend(legend, loc='best')
 
     fig.tight_layout()
     fig.savefig(out)
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("need csv file as cli parameter")
-        sys.exit(1)
+    # if len(sys.argv) < 2:
+    #     print("need csv file as cli parameter")
+    #     sys.exit(1)
+    data = list()
 
-    data = np.loadtxt(sys.argv[1], delimiter=',')
+    data.append(('static', np.loadtxt('weak_scaling.csv', delimiter=',')))
+    data.append(('1-0.25', np.loadtxt('weak_scaling_dynsched.csv', delimiter=',')))
 
     plot_time(data, 'weak_scaling_time.pdf')
 
