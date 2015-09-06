96b638ba39994f91f65c8f7be0274ec117b1dc21
Lennart Braun <3braun@informatik.uni-hamburg.de>
2015-09-06 21:58:27 +0200

    cleaned up plotting script

--- code/data/plot_strong_scaling.py
+++ code/data/plot_strong_scaling.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
 
-# import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def plot_time(data, out):
-    speedup = [d[1][0, 2] / d[1][:, 2] for d in data]
-    efficiency = [s / d[1][:, 0] for d, s in zip(data, speedup)]
+    speedup = [d['data'][0, 2] / d['data'][:, 2] for d in data]
+    efficiency = [s / d['data'][:, 0] for d, s in zip(data, speedup)]
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(12, 5))
 
     # speedup per process
-    ax_sp = fig.add_subplot(2, 1, 1)
+    ax_sp = fig.add_axes([0.075, 0.55, 0.75, 0.375])
+    ax_sp.plot(data[0]['data'][:, 0], data[0]['data'][:, 0],
+               label='ideal', color='grey')
     for d, s in zip(data, speedup):
-        ax_sp.plot(d[1][:, 0], s, 'x')
-    ax_sp.plot(data[0][1][:, 0], data[0][1][:, 0])
-    ax_sp.set_title("Speedup")
-    ax_sp.set_xlabel("# Processes")
+        ax_sp.plot(d['data'][:, 0], s, '+', label=d['label'], color=d['color'])
     ax_sp.set_ylabel("Speedup")
-    legend = [d[0] for d in data]
-    legend.append('ideal speedup')
-    ax_sp.legend(legend, loc='best')
+    h_sp, l_sp = ax_sp.get_legend_handles_labels()
+    ax_sp.tick_params(labelright=True)
 
     # efficiency per process
-    ax_ef = fig.add_subplot(2, 1, 2)
+    ax_ef = fig.add_axes([0.075, 0.1, 0.75, 0.375])
     for d, e in zip(data, efficiency):
-        ax_ef.plot(d[1][:, 0], e, 'x')
-    ax_ef.set_title("Efficiency")
+        ax_ef.plot(d['data'][:, 0], e, '+', color=d['color'])
     ax_ef.set_xlabel("# Processes")
     ax_ef.set_ylabel("Efficiency")
-    legend = [d[0] for d in data]
-    ax_ef.legend(legend, loc='best')
+    h_ef, l_ef = ax_ef.get_legend_handles_labels()
+    ax_ef.tick_params(labelright=True)
 
-    fig.tight_layout()
+    fig.legend(h_sp + h_ef, l_sp + l_ef, loc=(0.875, 0.1))
+
+    # fig.tight_layout()
     fig.savefig(out, papertype='a4')
 
 
@@ -57,31 +55,55 @@ def plot_moves(data, out):
 
 
 def main():
-    # if len(sys.argv) < 2:
-    #     print("need csv file as cli parameter")
-    #     sys.exit(1)
-
     data = list()
-    data.append(('static', np.loadtxt('strong_scaling.csv', delimiter=',')))
-    data.append(('10-0.5', np.loadtxt('strong_scaling_dynsched-10-05.csv',
-                                      delimiter=',')))
-    data.append(('5-0.5', np.loadtxt('strong_scaling_dynsched-5-05.csv',
-                                     delimiter=',')))
-    data.append(('1-0.5', np.loadtxt('strong_scaling_dynsched-1-05.csv',
-                                     delimiter=',')))
+    data.append({
+        'label': 'static',
+        'color': 'blue',
+        'data': np.loadtxt('strong_scaling.csv', delimiter=','),
+    })
+    plot_time(data, 'strong_scaling_time_static.pdf')
+    data.append({
+        'label': '1-0.5',
+        'color': 'red',
+        'data': np.loadtxt('strong_scaling_dynsched-1-05.csv', delimiter=','),
+    })
+    data.append({
+        'label': '5-0.5',
+        'color': 'orange',
+        'data': np.loadtxt('strong_scaling_dynsched-5-05.csv', delimiter=','),
+    })
+    data.append({
+        'label': '10-0.5',
+        'color': 'green',
+        'data': np.loadtxt('strong_scaling_dynsched-10-05.csv', delimiter=','),
+    })
     # data.append(('2-0.25', np.loadtxt('strong_scaling_dynsched-2-025.csv',
     #                                  delimiter=',')))
 
     data2 = list()
-    data2.append(('static', np.loadtxt('strong_scaling.csv', delimiter=',')))
-    data2.append(('1-0.50', np.loadtxt('strong_scaling_dynsched-1-05.csv',
-                                       delimiter=',')))
-    data2.append(('1-0.75', np.loadtxt('strong_scaling_dynsched-1-075.csv',
-                                       delimiter=',')))
-    data2.append(('1-0.25', np.loadtxt('strong_scaling_dynsched-1-025.csv',
-                                       delimiter=',')))
-    data2.append(('1-0.00', np.loadtxt('strong_scaling_dynsched-1-00.csv',
-                                       delimiter=',')))
+    data2.append({
+        'label': 'static',
+        'color': 'blue',
+        'data': np.loadtxt('strong_scaling.csv', delimiter=','),
+    })
+    data2.append({
+        'label': '1-0.25',
+        'color': 'red',
+        'data': np.loadtxt('strong_scaling_dynsched-1-025.csv', delimiter=','),
+    })
+    plot_time(data2, 'strong_scaling_time_final.pdf')
+    data2.append({
+        'label': '1-0.50',
+        'color': 'orange',
+        'data': np.loadtxt('strong_scaling_dynsched-1-05.csv', delimiter=','),
+    })
+    data2.append({
+        'label': '1-0.75',
+        'color': 'green',
+        'data': np.loadtxt('strong_scaling_dynsched-1-075.csv', delimiter=','),
+    })
+    # data2.append(('1-0.00', np.loadtxt('strong_scaling_dynsched-1-00.csv',
+    #                                    delimiter=','), 'grey'))
     # data2.append(('1-0.375',
     #               np.loadtxt('strong_scaling_dynsched-1-0375.csv', delimiter=',')))
     # data2.append(('1-0.125',
